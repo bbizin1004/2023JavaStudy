177a13c1a5e34f67e901a1e8d6c53c3c7976bb01
bbizin1004 <bbizin1004@korea.com>
2024-01-03 18:07:13 +0900

    1월 3일 작성

---
+++ src/ex08class/PersonConstructor.java
@@ -0,0 +1,16 @@
+package ex08class;
+
+public class PersonConstructor {
+
+	public static void main(String[] args) {
+		
+		String name;
+		int age;
+		String addr;
+		
+		//생성자 메소드
+		//public P
+
+	}
+
+}
