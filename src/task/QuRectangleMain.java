1ec97e305d8719dd51322ba2bf2dc4af219289fb
bbizin1004 <bbizin1004@korea.com>
2024-01-10 10:04:11 +0900

    학원에서 작업끝

---
+++ src/task/QuRectangleMain.java
@@ -0,0 +1,42 @@
+package task;
+
+//직사각형을 표현한 클래스
+class Rectangle {
+	int width;
+	int height;
+
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public void showAreaInfo() {
+		System.out.println("직사각형 면적:" + width * height);
+	}
+
+}
+
+//정사각형을 표현한 클래스(정사각형은 직사각형의 일종)
+class Square extends Rectangle {
+
+	public Square(int width) {
+		super(width, width);
+
+	}
+
+	@Override
+	public void showAreaInfo() {
+		System.out.println("정사각형 면적:" + width * height);
+	}
+
+}
+
+class QuRectangleMain {
+	public static void main(String[] args) {
+		Rectangle rec = new Rectangle(4, 3);
+		rec.showAreaInfo();
+
+		Square sqr = new Square(7);
+		sqr.showAreaInfo();
+	}
+}
